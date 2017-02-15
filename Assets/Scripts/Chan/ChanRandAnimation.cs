cbeeae31918f29bfddf384aabd40ea836fe57349
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-02-15 02:02:34 +0100

    Jumping Chan

---
+++ Assets/Scripts/Chan/ChanRandAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChanRandAnimation : MonoBehaviour
+{
+	public Animator anim;
+
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			anim.Play ("JUMP00", -1, 0f);
+		}
+//		if (Input.GetKeyDown ("1")) {
+//			anim.Play ("JUMP00", -1, 0f);
+//		} else if (Input.GetKeyDown ("2")) {
+//			anim.Play ("JUMP01", -1, 0f);
+//		} else if (Input.GetKeyDown ("3")) {
+//			anim.Play ("WAIT02", -1, 04);
+//		} else if (Input.GetKeyDown ("4")) {
+//			anim.Play ("WAIT03", -1, 0f);
+//		} else if (Input.GetKeyDown ("5")) {
+//			anim.Play ("WAIT04", -1, 0f);
+//		}
+	}
+}
