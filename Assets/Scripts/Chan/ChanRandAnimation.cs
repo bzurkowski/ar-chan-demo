dc78b36d6c7dfda297b022720908788c376b58a5
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-02-16 13:57:39 +0100

    Add more animations

--- Assets/Scripts/Chan/ChanRandAnimation.cs
+++ Assets/Scripts/Chan/ChanRandAnimation.cs
@@ -14,18 +14,28 @@ public class ChanRandAnimation : MonoBehaviour
 	void Update ()
 	{
 		if (Input.GetMouseButtonDown (0)) {
+			anim.Play ("DAMAGED00", -1, 0f);
+		} else if (Input.GetKeyDown ("1")) {
+			anim.Play ("WAIT02", -1, 04);
+		} else if (Input.GetKeyDown ("2")) {
+			anim.Play ("WAIT03", -1, 0f);
+		} else if (Input.GetKeyDown ("3")) {
+			anim.Play ("WAIT04", -1, 0f);
+		} else if (Input.GetKeyDown ("4")) {
+			anim.Play ("WALK00_F", -1, 0f);
+		} else if (Input.GetKeyDown ("5")) {
+			anim.Play ("RUN00_F", -1, 0f);
+		} else if (Input.GetKeyDown ("6")) {
+			anim.Play ("SLIDE00", -1, 0f);
+		} else if (Input.GetKeyDown ("7")) {
+			anim.Play ("WIN00", -1, 0f);
+		} else if (Input.GetKeyDown ("8")) {
+			anim.Play ("LOSE00", -1, 0f);
+		} else if (Input.GetKeyDown ("9")) {
 			anim.Play ("JUMP00", -1, 0f);
+		} else if (Input.GetKeyDown ("0")) {
+			anim.Play ("HANDUP00_R", -1, 0f);
 		}
-//		if (Input.GetKeyDown ("1")) {
-//			anim.Play ("JUMP00", -1, 0f);
-//		} else if (Input.GetKeyDown ("2")) {
-//			anim.Play ("JUMP01", -1, 0f);
-//		} else if (Input.GetKeyDown ("3")) {
-//			anim.Play ("WAIT02", -1, 04);
-//		} else if (Input.GetKeyDown ("4")) {
-//			anim.Play ("WAIT03", -1, 0f);
-//		} else if (Input.GetKeyDown ("5")) {
-//			anim.Play ("WAIT04", -1, 0f);
-//		}
+		
 	}
 }
